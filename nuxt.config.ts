a69465b3415482ba93d777613593d8ab28d08c4f
Hover19 <klaskintrade@gmail.com>
2025-01-13 14:55:49 +0200

    add employee card and implementation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import svgLoader from "vite-svg-loader";
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  css: ["@/styles/main.scss"],
+  css: ["@/styles/main.scss", "@/styles/_mixins.scss"],
   vite: {
     plugins: [svgLoader()],
   },
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,88 @@
 <template>
-  <div>
-    <h1>Home Page</h1>
-    <p>Welcome to the About Page of our Nuxt 3 app!</p>
+  <div class="employee-section">
+    <button
+      @click="previousEmployee"
+      :disabled="currentIndex === 0"
+      class="arrow"
+    >
+      &larr;
+    </button>
+
+    <EmployeeCard :employee="employees[currentIndex]" />
+
+    <button
+      @click="nextEmployee"
+      :disabled="currentIndex === employees.length - 1"
+      class="arrow"
+    >
+      &rarr;
+    </button>
   </div>
 </template>
+
+<script setup>
+import { ref } from "vue";
+import EmployeeCard from "@/components/EmployeeCard.vue";
+
+const employees = [
+  {
+    photo: "/assets/employee.png",
+    name: "John",
+    position: "Waiter",
+    tips: 0,
+  },
+  {
+    photo: "/assets/employee.png",
+    name: "Jane",
+    position: "Waitress",
+    tips: 0,
+  },
+  {
+    photo: "/assets/employee.png",
+    name: "Mike",
+    position: "Bartender",
+    tips: 0,
+  },
+  {
+    photo: "/assets/employee.png",
+    name: "Emily",
+    position: "Manager",
+    tips: 0,
+  },
+];
+
+const currentIndex = ref(0);
+
+const previousEmployee = () => {
+  if (currentIndex.value > 0) currentIndex.value--;
+};
+
+const nextEmployee = () => {
+  if (currentIndex.value < employees.length - 1) currentIndex.value++;
+};
+</script>
+
+<style scoped>
+.employee-section {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 16px;
+  gap: 30px;
+}
+
+.arrow {
+  background-color: var(--violet-color);
+  color: var(--white-color);
+  border: none;
+  border-radius: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+  font-size: 1rem;
+}
+
+.arrow:disabled {
+  background-color: #ddd;
+  cursor: not-allowed;
+}
+</style>
